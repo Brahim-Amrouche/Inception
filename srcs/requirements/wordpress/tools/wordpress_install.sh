536aa74f6bac58723474094d444f9a22e328d865
Brahim-Amrouche <amroucheb4@gmail.com>
2024-03-17 22:51:42 +0000

    wordpress completed and setup with fpm just need ARGS , now up to nginx

--- srcs/requirements/wordpress/tools/wordpress_install.sh
+++ srcs/requirements/wordpress/tools/wordpress_install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo ">> Configuring Wordpress <<" 
 wp --allow-root config create --dbname=Test --dbuser=bamrouch --dbpass=adm123 --dbhost=mariadb:3306 --dbcharset=utf8mb4
+
+echo ">> Installing Wordpress <<"
 wp --allow-root core install --url=test.com --title=Test --admin_user=bamrouch_admin --admin_password=adm123 --admin_email=amroucheb4@gmail.com
 
+echo ">> Wordpress Finished Installing <<"
+
+echo ">> Launching php-fpm <<"
+php-fpm8.2
--- wordpress_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wp --allow-root config create --dbname=Test --dbuser=bamrouch --dbpass=adm123 --dbhost=mariadb:3306 --dbcharset=utf8mb4
-wp --allow-root core install --url=test.com --title=Test --admin_user=bamrouch_admin --admin_password=adm123 --admin_email=amroucheb4@gmail.com
-
