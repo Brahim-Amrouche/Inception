776b14d563c9cbe057bd9113edfbf39014f4670a
Brahim-Amrouche <amroucheb4@gmail.com>
2024-03-18 15:07:36 +0000

    transforming wordpress args into environement variables

--- srcs/requirements/wordpress/tools/wordpress_install.sh
+++ srcs/requirements/wordpress/tools/wordpress_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo ">> Configuring Wordpress <<" 
-wp --allow-root config create --dbname="$sql_db_name" --dbuser="$mariadb_user" --dbpass="$mariadb_user_password" --dbhost=mariadb:3306 --dbcharset=utf8mb4
+echo ">> Configuring Wordpress <<" $sql_db_name "||"  $mariadb_user "||" $mariadb_user_password 
+wp --allow-root config create --dbname=$sql_db_name --dbuser=$mariadb_user --dbpass=$mariadb_user_password --dbhost=mariadb:3306 --dbcharset=utf8mb4
 
-echo ">> Installing Wordpress <<"
-wp --allow-root core install --url="$wordpress_url" --title="$wordpress_website_title" --admin_user="$wordpress_admin_user" --admin_password="$wordpress_admin_password" --admin_email="$wordpress_admin_email"
+echo ">> Installing Wordpress <<" $wordpress_url "||" $wordpress_website_title "||" $wordpress_admin_user "||" $wordpress_admin_user "||" $wordpress_admin_password "||" $wordpress_admin_email
+wp --allow-root core install --url=$wordpress_url --title=$wordpress_website_title --admin_user=$wordpress_admin_user --admin_password=$wordpress_admin_password --admin_email=$wordpress_admin_email
 
 echo ">> Wordpress Finished Installing <<"
 
