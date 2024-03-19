1123934a4f5d57ad7816ff8a9712c720b4823cc5
Brahim-Amrouche <amroucheb4@gmail.com>
2024-03-19 18:29:29 +0000

    before fixing nginx

--- srcs/requirements/nginx/tools/nginx_conf.sh
+++ srcs/requirements/nginx/tools/nginx_conf.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERTIFICATE_KEY_PATH -out $CERTIFICATE_PATH
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERTIFICATE_KEY_PATH -out $CERTIFICATE_PATH -subj "/C=ma/ST=Rhamna/L=Benguerir/O=1337/CN=$WORDPRESS_URL"
+
+
+export SCRIPTS_PATH='$document_root$fastcgi_script_name'
+export ARGS='$args'
+export URI='$uri'
+
+envsubst < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/defaul.conf
 
 nginx -g 'daemon off;'
