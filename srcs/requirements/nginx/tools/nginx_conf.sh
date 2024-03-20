316ef3931b1d7909d70acf3d38aa4a4c01af2afa
Brahim-Amrouche <amroucheb4@gmail.com>
2024-03-20 18:40:18 +0000

    About serving wordpress through nginx

--- srcs/requirements/nginx/tools/nginx_conf.sh
+++ srcs/requirements/nginx/tools/nginx_conf.sh
@@ -3,7 +3,7 @@
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERTIFICATE_KEY_PATH -out $CERTIFICATE_PATH -subj "/C=ma/ST=Rhamna/L=Benguerir/O=1337/CN=$WORDPRESS_URL"
 
 
-export SCRIPTS_PATH='$document_root$fastcgi_script_name'
+export SCRIPTS_PATH='./$fastcgi_script_name'
 export ARGS='$args'
 export URI='$uri'
 
