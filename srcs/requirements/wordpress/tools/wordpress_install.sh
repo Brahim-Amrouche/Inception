48be022163913b94d33cbbd5299ec4484f0300dd
Brahim-Amrouche <amroucheb4@gmail.com>
2024-03-20 22:53:31 +0000

    removed chdir and it only make things worse

--- srcs/requirements/wordpress/tools/wordpress_install.sh
+++ srcs/requirements/wordpress/tools/wordpress_install.sh
@@ -8,6 +8,8 @@ wp --allow-root config create --dbname=$SQL_DB_NAME --dbuser=$MARIADB_USER --dbp
 echo ">> Installing Wordpress << $WORDPRESS_URL with tile of $WORDPRESS_WEBSITE_TITLE and $WORDPRESS_ADMIN_USER with $WORDPRESS_ADMIN_PASSWORD and email of $WORDPRESS_ADMIN_EMAIL" 
 wp --allow-root core install --url=$WORDPRESS_URL --title=$WORDPRESS_WEBSITE_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL
 
+chown -R www-data:www-data /var/www/wordpress
+
 echo ">> Wordpress Finished Installing <<"
 
 echo ">> Launching php-fpm <<"
